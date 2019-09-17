93777e24b02bfe9489896445ccab1a6aad510acc
JK-97 <benjamin-dream@outlook.com>
2019-09-17 21:12:20 +0800

    add driver config api

---
+++ pkg/jxstartup/bootstrap.go
@@ -0,0 +1,71 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+//
+// Copyright (C) 2017-2018 Canonical Ltd
+// Copyright (C) 2018-2019 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package jxstartup
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/edgexfoundry/device-sdk-go"
+	dsModels "github.com/edgexfoundry/device-sdk-go/pkg/models"
+)
+
+var (
+	confProfile string
+	confDir     string
+	useRegistry string = "consul://172.17.0.1:8500"
+
+	Service *device.Service
+	err     error
+)
+
+func init() {
+	flag.StringVar(&useRegistry, "registry", useRegistry, "Indicates the service should use the registry and provide the registry url.")
+	flag.StringVar(&useRegistry, "r", useRegistry, "Indicates the service should use registry and provide the registry path.")
+	flag.StringVar(&confProfile, "profile", confProfile, "Specify a profile other than default.")
+	flag.StringVar(&confProfile, "p", confProfile, "Specify a profile other than default.")
+	flag.StringVar(&confDir, "confdir", confDir, "Specify an alternate configuration directory.")
+	flag.StringVar(&confDir, "c", confDir, "Specify an alternate configuration directory.")
+}
+
+func StartService(serviceName string, serviceVersion string, driver dsModels.ProtocolDriver) error {
+	if !flag.Parsed() {
+		flag.Parse()
+	}
+
+	Service, err = device.NewService(serviceName, serviceVersion, confProfile, confDir, useRegistry, driver)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(os.Stdout, "Calling service.Start.\n")
+	errChan := make(chan error, 2)
+	listenForInterrupt(errChan)
+
+	err = Service.Start(errChan)
+	if err != nil {
+		return err
+	}
+	go listenForConfigChanges()
+
+	err = <-errChan
+	fmt.Fprintf(os.Stdout, "Terminating: %v.\n", err)
+
+	return Service.Stop(false)
+}
+
+func listenForInterrupt(errChan chan error) {
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+}
---
+++ pkg/jxstartup/config.go
@@ -0,0 +1,50 @@
+package jxstartup
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/edgexfoundry/device-sdk-go/internal/common"
+	"github.com/edgexfoundry/device-sdk-go/internal/config"
+)
+
+const driverKey = "Driver"
+
+func PutDriverConfig(name string, value []byte) error {
+	return config.RegistryClient.PutConfigurationValue(driverKey+"/"+name, value)
+}
+
+func listenForConfigChanges() {
+	errChannel := make(chan error)
+	updateChannel := make(chan interface{})
+
+	// common.CurrentConfig.Driver need to be updated manually,
+	// WatchForChanges will not update for us
+	config.RegistryClient.WatchForChanges(updateChannel, errChannel, &map[string]string{}, driverKey)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	for {
+		select {
+		case <-signalChan:
+			// Quietly and gracefully stop when SIGINT/SIGTERM received
+			return
+		case ex := <-errChannel:
+			common.LoggingClient.Error(ex.Error())
+		case raw, ok := <-updateChannel:
+			if ok {
+				newDriver, ok := raw.(*map[string]string)
+				if !ok {
+					common.LoggingClient.Error("listenForConfigChanges() type check failed")
+				}
+				// TODO: call callback
+				common.CurrentConfig.Driver = *newDriver
+				common.LoggingClient.Info("Driver configuration has been updated.")
+			} else {
+				return
+			}
+		}
+	}
+}
